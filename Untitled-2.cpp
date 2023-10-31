290bc7c10758ec0c0611cfdf64afc8df75589184
mykolahurskyi <mykola.hurskyi.shi.2023@lpnu.ua>
2023-10-31 11:21:41 +0300

    first

---
+++ Untitled-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+
+
+int main(){
+    std::string name 
+    double pricipal, rate, total_amount ;
+    int years, month, year, quarter ;
+
+    std::cout <<"Enter your name: " ;
+    std::cin >> name;
+    std::cout <<"Enter the principal amount:";
+    std::cin >> pricipal; 
+    std::cout <<"Enter the principical amount: ";
+
+    std::cout << "Enter the rate of interest (in percentage):";
+    std::cin >> rate;
+
+    std::cout <<"Enter the number of years: ";
+    std::cin >> year;
+
+    std::cout <<"Enter the number of times interest in compounded per year: )";
+    std::cin >> month
+
+    std::count << "Enter "
+}
+
+
